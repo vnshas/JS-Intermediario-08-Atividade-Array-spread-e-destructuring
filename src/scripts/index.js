c49f66b1b11aefef831c8a1b479f73fce5f408cb
vnshas <vnshas@gmail.com>
2024-12-10 13:45:25 -0300

    a

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,13 +6,14 @@ import usuarios from "./dbUsers.js"
 import enderecos from "./dbAddress.js"
 
 // Faça as importações corretas acima
-console.log(usuarios);
-console.log(enderecos);
+//console.log(usuarios);
+//console.log(enderecos);
 
 // Adicionar endereços aos usuarios pelo id
 
 function addAddresToUser(listaDeUsuarios, listaDeEnderecos) {
     const userWithAddress = listaDeUsuarios.map((user) =>{
+      
       const { id } = user
       const { endereco } = listaDeEnderecos.find((currentAddress) =>{
         return currentAddress.userId == id
@@ -37,18 +38,22 @@ function deletePassword({password,...rest}) {
 // Listar os usuarios sem a senha
 function listUsers(usuarios) {
   return usuarios.map((user) => deletePassword(user))
-}
- 
- console.log(listUsers(usuarios));
+  
+ }
+
+console.log(listUsers(usuarios));
 
 // Listar somente nome, email e stack
 function listUserStacks(listaDeUsuarios) {
-  return;
-}
-// console.log(listUserStacks(usuarios));
+  return listaDeUsuarios.map(({nome, email, stacks}) =>({
+    nome,
+    email,
+    stacks
+  }))}
+console.log(listUserStacks(usuarios));
 
 //Deletar um usuário que tenha python na stack
 function deleteUser(listaDeUsuarios) {
-  return;
+  return listaDeUsuarios.filter(({stacks}) => !stacks.includes("Python")).map((user) => deletePassword(user)) 
 }
-// console.log(deleteUser(usuarios));
+console.log(deleteUser(usuarios));
